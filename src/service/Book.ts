c7bf41704a835954334e27f4a14e71849a6ab51b
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-11-11 19:17:38 +0100

    Add collection documentation

--- src/service/Book.ts
+++ src/service/Book.ts
@@ -9,14 +9,27 @@ export class Books {
     this.map = {};
   }
 
+  /**
+   * Return all books stored in the collection.
+   */
   getAll(): Book[] {
     return Object.values(this.map);
   }
 
+  /**
+   * Add a book in the collection.
+   * @param book Book to add.
+   */
   add(book: Book): void {
     this.map[book._id] = book;
   }
 
+  /**
+   * Update a book stored in the collection.
+   * @param id Book id to update.
+   * @param name New book name.
+   * @param author New book author.
+   */
   update(id: string, name: string, author: string): void {
     const book: Book = this.get(id);
 
@@ -24,10 +37,18 @@ export class Books {
     book.author = author;
   }
 
+  /**
+   * Retrieve a book based on its id.
+   * @param id Book id to retrieve.
+   */
   get(id: string): Book {
     return this.map[id];
   }
 
+  /**
+   * Drop a book based on its id.
+   * @param id Book id to drop.
+   */
   drop(id: string): void {
     delete this.map[id];
   }
